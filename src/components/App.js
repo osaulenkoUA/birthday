508bb2c8111eb56190e7a1997cdb15f64ce8704c
Oleksandr <oom2212@gmail.com>
2021-07-23 12:05:29 +0300

    add more img

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import param from './param.js';
 export default function App() {
   return (
     <div className={s.container}>
-      <h2 className={s.title}>Happy Birthday !!!</h2>
+      <h2 className={s.title}>Happy Birthday Valentine !!!</h2>
       {param.map(item => (
         <Balloon key={Math.random()} param={item} />
       ))}
--- src/components/Balloon/ballon.js
+++ src/components/Balloon/ballon.js
@@ -1,29 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import rose from './rose.png';
+import React, {useEffect, useState} from 'react';
 import s from './balloon.module.css';
 import Svg from '../assets/svg.js';
 
-export default function App({ param }) {
-    const { w, h, d } = param;
-  const [state, setstate] = useState(false);
-  const [height, setHeight] = useState(window.innerHeight-h);
-  useEffect(() => {
-    const id = setTimeout(() => {
-      setHeight(height - d);
-    }, 20);
-    if (height < -215) setHeight(window.innerHeight);
-    return () => clearTimeout(id);
-  }, [height]);
+import img1 from '../../dataImg/img1.png';
+import img3 from '../../dataImg/img3.png';
+import img4 from '../../dataImg/img4.png';
+import img5 from '../../dataImg/img5.png';
+import img6 from '../../dataImg/img6.png';
+import img7 from '../../dataImg/img7.png';
+import img8 from '../../dataImg/img8.png';
+import img9 from '../../dataImg/img9.png';
+import img10 from '../../dataImg/img10.png';
+import img11 from '../../dataImg/img11.png';
+import img2 from '../../dataImg/img2.png';
 
-  function foo() {
-      setstate(true);
-  }
-  return (
-    <>
-      <div className={s.wrap_img} style={{ transform: `translateY(${height}px)`, left: w }}>
-        {!state && <Svg foo={foo} />}
-        {state && <img src={rose} alt={'img'}/>}
-      </div>
-    </>
-  );
+export default function App({param}) {
+    const {w, h, d,number} = param;
+    const [state, setstate] = useState(false);
+    const [height, setHeight] = useState(window.innerHeight - h);
+
+    const dataImg = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11];
+
+    useEffect(() => {
+        const id = setTimeout(() => {
+            setHeight(height - d);
+        }, 20);
+        if (height < -215) setHeight(window.innerHeight);
+        return () => clearTimeout(id);
+    }, [height]);
+
+    function foo() {
+        setstate(true);
+    }
+
+
+    return (
+        <>
+            <div className={s.wrap_img} style={{transform: `translateY(${height}px)`, left: w}}>
+                {!state && <Svg foo={foo}/>}
+                {state && <img src={dataImg[number]} alt={'img'}/>}
+            </div>
+        </>
+    );
 }
